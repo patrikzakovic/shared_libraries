f2eeb20f1c08ef7527bac0f36a9c750b01c902ef
432135 <zakovic.patrik@slsp.sk>
2022-09-12 13:09:39 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="shared_libraries",
+    version="0.1",
+    install_requires=[
+        'robotframework>=5.0.0',
+        'robotframework-databaselibrary>=1.0.1'
+    ]
+)
